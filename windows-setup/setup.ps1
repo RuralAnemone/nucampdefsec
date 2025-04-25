b65488618c41de4690daaaf380ebf28a7ea31f61
DavidHoenisch <dh1689@pm.me>
2025-04-24 23:19:51 -0700

    Add blank line between Chocolatey installation and packages

--- windows-setup/setup.ps1
+++ windows-setup/setup.ps1
@@ -3,6 +3,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+
 # Install VirtualBox and Multipass via Chocolatey
 choco install virtualbox -y
 choco install multipass --params="'/HyperVisor:VirtualBox'" -y
